d6913f5500193224d4b5dac5a5f2bb758048a266
VaishnaviMNGTS <vaishnavi.mishra@ngtsindore.com>
2025-10-13 20:47:00 +0530

    fix

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,24 +1,40 @@
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 export const About = (props) => {
+  useEffect(() => {
+    AOS.init({ duration: 1000, once: true });
+  }, []);
+
   return (
-    <div id="about" className="py-5">
+    <div id="" className="about-section py-5">
       <div className="container-fluid px-3 px-md-5">
         <div className="row align-items-center">
           {/* Left Image */}
-          <div className="col-12 col-md-6 mb-4 mb-md-0 text-center">
-            <img
-              src="img/front1.webp"
-              className="img-fluid w-100 rounded shadow-sm"
-              alt="About Hanuman Action Art"
-              style={{ maxHeight: "500px", objectFit: "cover" }}
-            />
+          <div
+            className="col-12 col-md-6 mb-4 mb-md-0 text-center"
+            data-aos="fade-right"
+          >
+            <div className="image-wrapper">
+              <img
+                src="img/front1.webp"
+                className="img-fluid w-100 rounded shadow-lg"
+                alt="About Hanuman Action Art"
+              />
+            </div>
           </div>
 
           {/* Right Text */}
-          <div className="col-12 col-md-6">
-            <div className="">
+          <div className="col-12 col-md-6" data-aos="fade-left">
+            <div className="about-text">
               <h2 className="mb-3">About Us</h2>
               <p>{props.data ? props.data.paragraph1 : "Loading..."}</p>
               <p>{props.data ? props.data.paragraph2 : "Loading..."}</p>
+
+              <a href="#services" className="btn btn-custom mt-3">
+                Learn More →
+              </a>
             </div>
           </div>
         </div>
